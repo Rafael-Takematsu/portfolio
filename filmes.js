4ad00f7d70680af30411812b9117740764c1f3b8
Rafael-Takematsu <rafatakematsu@gmail.com>
2025-11-16 17:29:30 -0300

    filmes prontos

---
+++ filmes.js
@@ -0,0 +1,30 @@
+let slideIndex = 0;
+const bgImages = [
+    'url(images/gotg-bg.webp)',
+    'url(images/lotr-bg.jpg)',
+    'url(images/starwars-bg.jpg)'
+];
+
+showSlides();
+
+function showSlides() {
+    let i;
+    let slides = document.getElementsByClassName("trilogia");
+    
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    
+    if (slideIndex > slides.length) {
+        slideIndex = 1;
+    }
+    
+    if (bgImages[slideIndex - 1]) {
+        slides[slideIndex - 1].style.backgroundImage = bgImages[slideIndex - 1];
+    }
+    
+    slides[slideIndex - 1].style.display = "block";
+    
+    setTimeout(showSlides, 8000);
+}
